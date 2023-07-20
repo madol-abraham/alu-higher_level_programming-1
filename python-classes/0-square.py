1b89b077a561b405a496ef26fc6121deea055558
John-Akech <j.akech@alustudent.com>
2023-07-20 13:37:17 -0700

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-'''Define a class Square.'''
+'''Defining a class Square.'''
 
 
 class Square:
-    '''Represent a square.'''    
+    '''Representing a square.'''
     pass
+
