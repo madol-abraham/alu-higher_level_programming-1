dfe36f81997ce447090ef17bf1b2e5b9a64e4f0b
John-Akech <j.akech@alustudent.com>
2023-07-20 14:30:37 -0700

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,8 +7,11 @@ class Square:
     '''Representing a new Square.'''
 
     def __init__(self, size):
-        '''Initializing a new class.'''
-
+        '''Initializing a new class.
+        
+        Args:
+            size (int): The size of the new square.
+        '''
         if no isinstance(self, int):
             raise TypeError("size must be an integer")
         elif size < 0:
