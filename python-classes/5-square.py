7e0eb09c09bd7348cd8a21ca64652f121ef6c299
John-Akech <j.akech@alustudent.com>
2023-07-20 15:24:46 -0700

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -31,6 +31,7 @@ class Square:
         '''Return the current area of the square.'''
         return (self.__size * self.__size)
     def my_print(self):
+
         '''Print the square with the # character.'''
         for i in range(0, self.__size):
             [print('#', end='') for j in range(self.__size)]
