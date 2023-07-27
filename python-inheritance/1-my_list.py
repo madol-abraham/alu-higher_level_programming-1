e91f09122acbcc38041a42a346484b24d1414469
John-Akech <j.akech@alustudent.com>
2023-07-27 10:29:41 -0700

    My List

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,14 @@
-class MyList(list):
-    def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+
+print(my_list)         # Output: [1, 4, 2, 3, 5]
+my_list.print_sorted() # Output: [1, 2, 3, 4, 5]
+print(my_list)         # Output: [1, 4, 2, 3, 5]
 
