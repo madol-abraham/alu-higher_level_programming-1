c5c56cc2e875966326afbf4840b9effc3033025b
John-Akech <j.akech@alustudent.com>
2023-08-15 01:57:12 -0700

    tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -13,7 +13,7 @@ def matrix_divided(matrix, div):
         div: divisor
     """
     if type(div) is not float and type(div) is not int:  # check div type
-        raise TypeError('div must be a number')   
+        raise TypeError('div must be a number')
     if div == 0:
         raise ZeroDivisionError('division by zero')
 
@@ -21,21 +21,14 @@ def matrix_divided(matrix, div):
     ref_row = 0
     if x is not 0 and type(matrix[x - 1]) is list and len(matrix[x - 1]) > 0:
         ref_row = len(matrix[x - 1])  # use last row as reference
-        n_matrix = [[] for row in range(x)]
-    else:
-        raise TypeError('matrix must be a matrix \
-(list of lists) of integers/floats')
+    n_matrix = [[] for row in range(x)]  # Create new matrix with empty rows
 
-    for row, n_row in zip(matrix, n_matrix):  # check element types
-        if type(row) is not list:  # check that list member is list
-            raise TypeError('matrix must be a matrix \
-(list of lists) of integers/floats')
-        if len(row) != ref_row:  # check for uniform rows
+    for row in range(x):
+        if len(matrix[row]) != ref_row:
             raise TypeError('Each row of the matrix must have the same size')
-        for elem in row:
-            if type(elem) is not float and type(elem) is not int:  # check types
-                raise TypeError('matrix must be a matrix \
-(list of lists) of integers/floats')
-            n_row.append(round(float(elem / div), 2))  # divide elements by div
+        for item in matrix[row]:
+            if type(item) is not float and type(item) is not int:
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            n_matrix[row].append(round(item / div, 2))  # Append the quotient rounded to 2 decimal places
 
     return n_matrix
